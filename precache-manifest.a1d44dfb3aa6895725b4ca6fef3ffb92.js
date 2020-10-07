bcdb6538be7941a3c3889a3e865fae0453840ba2
James McCann <jamesredux@gmail.com>
2020-10-07 19:18:56 +0100

    Updates

---
+++ precache-manifest.a1d44dfb3aa6895725b4ca6fef3ffb92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556d681c1325045b1463ae1fac17c454",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "fff3f56503b7ffe6e6ce",
+    "url": "/cv-project/static/css/main.de7607b6.chunk.css"
+  },
+  {
+    "revision": "4da558b3be53def3295d",
+    "url": "/cv-project/static/js/2.050fac93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-project/static/js/2.050fac93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff3f56503b7ffe6e6ce",
+    "url": "/cv-project/static/js/main.bf6800fa.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
